7f0a03bfaa13cf236c8f8120ed138167df0152f6
Vikhyat Gupta <vkhtgupta@gmail.com>
2024-11-04 22:22:22 +0530

    feature: Added create user API

--- users/apis/v1/user.py
+++ users/apis/v1/user.py
@@ -2,11 +2,19 @@
 This file contains all the APIs related to user model.
 """
 
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
 from rest_framework import serializers
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND
+from rest_framework.status import (
+    HTTP_200_OK,
+    HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+)
 from rest_framework.views import APIView
+
+User = get_user_model()
 
 
 class GenerateUserTokenAPI(APIView):
@@ -62,3 +70,48 @@ class GenerateUserTokenAPI(APIView):
             status=HTTP_200_OK,
             data={"token": user.token},
         )
+
+
+class CreateUserAPI(APIView):
+    """
+    This API is used to create user.
+    Response Codes:
+        201, 400
+    """
+
+    permission_classes = (AllowAny,)
+
+    class InputSerializer(serializers.Serializer):
+        """
+        This serializer is used to validate the input data.
+        """
+
+        email = serializers.EmailField()
+        username = serializers.CharField()
+        password = serializers.CharField()
+        first_name = serializers.CharField()
+        last_name = serializers.CharField(required=False)
+        profile_image = serializers.ImageField(required=False)
+
+    def post(self, request):
+        """
+        This method is used to create user.
+        """
+
+        serializer = self.InputSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(status=HTTP_400_BAD_REQUEST, data=serializer.errors)
+
+        validated_data = serializer.validated_data
+
+        user, _created = User.objects.get_or_create(
+            email=validated_data["email"],
+            username=validated_data["username"],
+            defaults={
+                "first_name": validated_data["first_name"],
+                "last_name": validated_data.get("last_name"),
+                "profile_image": validated_data.get("profile_image"),
+            },
+        )
+
+        return Response(status=HTTP_201_CREATED, data={"token": user.token})
