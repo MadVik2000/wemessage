65e1ea88e534f05d8751d4004d1f61eaed56d4e3
Vikhyat Gupta <vkhtgupta@gmail.com>
2024-11-28 18:37:35 +0530

    Feature: added delete group service

--- groups/services/__init__.py
+++ groups/services/__init__.py
@@ -3,4 +3,5 @@ This file contains all the service imports for groups module.
 """
 
 from .create import bulk_create_group_messages, create_group, create_group_admin
+from .delete import delete_group
 from .update import update_group
